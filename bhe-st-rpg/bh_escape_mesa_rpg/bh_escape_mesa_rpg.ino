ef3a1f1310928f718c0b74251432336f69aa886e
Carlos Ribeiro <carribeiro@gmail.com>
2021-11-30 21:38:43 -0300

    Corrigido código de inicialização

--- bhe-st-rpg/bh_escape_mesa_rpg/bh_escape_mesa_rpg.ino
+++ bhe-st-rpg/bh_escape_mesa_rpg/bh_escape_mesa_rpg.ino
@@ -42,11 +42,6 @@
 #define PN532_MOSI (11)
 #define PN532_MISO (12)
 
-// If using the breakout or shield with I2C, define just the pins connected
-// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
-#define PN532_IRQ   (2)
-#define PN532_RESET (3)  // Not connected by default on the NFC Shield
-
 // Uncomment just _one_ line below depending on how your breakout or shield
 // is connected to the Arduino:
 
@@ -57,8 +52,9 @@ Adafruit_PN532 nfc2(PN532_SCK, PN532_MISO, PN532_MOSI, 5);
 Adafruit_PN532 nfc3(PN532_SCK, PN532_MISO, PN532_MOSI, 6);
 
 #define NUM_ADAPTERS 4
-#define MAX_ADAPTERS 1
+#define MAX_ADAPTERS 4
 Adafruit_PN532 nfc[NUM_ADAPTERS] = {nfc0, nfc1, nfc2, nfc3};
+boolean nfc_found[NUM_ADAPTERS] = {false, false, false, false};
 
 String data[NUM_ADAPTERS];
 boolean dataUpdate = false;
@@ -72,28 +68,44 @@ void setup() {
   for ( int i = 0; i < MAX_ADAPTERS; i++ ) {
     Serial.println("Init Board " + String(i) + "...");
     nfc[i].begin();
-    //delay(500);
+
+    uint32_t versiondata = nfc[i].getFirmwareVersion();
+    if (! versiondata) {
+      Serial.print("Didn't find PN53x board");
+    }
+    else {
+      // Got ok data, print it out!
+      Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+      Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+      Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+      nfc_found[i] = true;
+      modela 
+      
+      // configure board to read RFID tags
+      nfc[i].SAMConfig();
+    }
   }
   
-  pinMode(RPG_RESET, INPUT_PULLUP);
-  pinMode(RPG_STATUS, OUTPUT);
+  // pinMode(RPG_RESET, INPUT_PULLUP);
+  // pinMode(RPG_STATUS, OUTPUT);
 }
 
 void reset() {
   // If the RPG_RESET pin is activated, clears the status to restart the game
   rpgStatus = false;
-  digitalWrite(RPG_STATUS, LOW);
+//  digitalWrite(RPG_STATUS, LOW);
 }
 
 void set_rpg_ok() {
   // game is solved, sthe the status pin HIGH and keep it until reset
   rpgStatus = true;
-  digitalWrite(RPG_STATUS, HIGH);
+//  digitalWrite(RPG_STATUS, HIGH);
 }
 
 void loop() {
   if (!rpgStatus) {
     for ( int i = 0; i < MAX_ADAPTERS; i++ ) {
+      if (!nfc_found[i]) continue;
       boolean success;
       uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
       uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
@@ -102,12 +114,12 @@ void loop() {
       // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
       // 'uid' will be populated with the UID, and uidLength will indicate
       // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-      Serial.print("r");
       success = nfc[i].readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength, 100);
     
       if (success) {
         Serial.println("Found a card!");
-        Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+        Serial.print("SS:" + String(i)+ " ");
+        Serial.print("UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
         Serial.print("UID Value: ");
         for (uint8_t i=0; i < uidLength; i++) {
           Serial.print(" 0x");Serial.print(uid[i], HEX); 
@@ -128,8 +140,8 @@ void loop() {
 
   // reads the status sign
   {
-    int rpgReset;
-    rpgReset = digitalRead(RPG_RESET);
+    int rpgReset = 1;
+    // rpgReset = digitalRead(RPG_RESET);
     if (!rpgReset) {
       reset();
     }
