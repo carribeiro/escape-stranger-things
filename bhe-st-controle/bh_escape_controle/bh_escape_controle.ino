f2ad2ca6f7aa20c9cc48241b8b479cea5095e7e0
Carlos Ribeiro <carribeiro@gmail.com>
2021-12-04 22:56:57 -0300

    Iniciando código da caixa de controle

---
+++ bhe-st-controle/bh_escape_controle/bh_escape_controle.ino
@@ -0,0 +1,23 @@
+
+#define ARVORE_GENEALOGICA_OK 2
+#define LIGA_TV 3
+#define RPG_OK 4
+#define RPG_RESET 5
+#define TRAVA_PORTA_ARMADILHA 6
+#define BOMBAS_OK 7
+#define SENSOR_PORTA_ARMADILHA 8
+#define BOMBAS_RESET 9
+#define BOMBAS_EFEITO 10
+#define CARTA_WILL_OK 11
+#define SENSOR_PORTA_ARMARIO 12
+#define TRAVA_PORTA_ARMARIO 13
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
